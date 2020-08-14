770792a8a7698e8a355b22e07919b4c7275db189
roht0515 <rodrigo051599@gmail.com>
2020-08-14 00:18:13 -0400

    GameConsole class created

---
+++ src/main/java/chess/GameConsole.java
@@ -0,0 +1,31 @@
+package chess;
+
+public class GameConsole {
+    private Player playerWhite;
+    private Player playerBlack;
+    private ChessBoard board;
+
+    public GameConsole() { }
+
+    /**Method that starts the game**/
+    public void start() {
+        System.out.println("the game has started");
+    }
+    /**Method that finish the game**/
+    public void over() {
+        System.out.println("the game has over");
+    }
+    /**Method that resets the game**/
+    public void reset() {
+        System.out.println("Reset game");
+    }
+    /**Method that show the check**/
+    public void showCheck() {
+        System.out.println("Check");
+    }
+    /**Method that prints or draws the board**/
+    public void drawChessBoard() {
+        System.out.println("board printed");
+    }
+
+}
