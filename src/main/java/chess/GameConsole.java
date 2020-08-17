9e6b23cbaa4d8175c821d9002a32978ca877ea18
Mirko Romay Ramos <mirko.romay@fundacion-jala.org>
2020-08-17 00:21:19 -0400

    Added to show chessboard at console

--- src/main/java/chess/GameConsole.java
+++ src/main/java/chess/GameConsole.java
@@ -5,7 +5,17 @@ public class GameConsole {
     private Player playerBlack;
     private ChessBoard board;
 
-    public GameConsole() { }
+    final static String LETTERS_ROW = "     a   b   c   d   e   f   g   h  \n";
+    final static String HYPHENS_ROW = "  ---------------------------------\n";
+    final static int LAST_INDEX = 7;
+    final static int FIRST_INDEX = 0;
+    final static String BLANK = " ";
+    final static String BAR = "|";
+    final static String END_LINE = "\n";
+
+    public GameConsole() {
+        this.board = new ChessBoard();
+    }
 
     /**Method that starts the game**/
     public void start() {
@@ -25,7 +35,22 @@ public class GameConsole {
     }
     /**Method that prints or draws the board**/
     public void drawChessBoard() {
-        System.out.println("board printed");
+        String container;
+        container = LETTERS_ROW;
+        container += HYPHENS_ROW;
+        for (int irow = LAST_INDEX; irow >= FIRST_INDEX; irow--) {
+            container += (irow + 1) + BLANK + BAR ;
+            for (int icol = FIRST_INDEX; icol <= LAST_INDEX; icol++) {
+                if (this.board.getBoard()[icol][irow].getColor() == Color.BLACK) {
+                    container += BLANK + this.board.getBoard()[icol][irow].toString() + BAR;
+                } else {
+                    container += this.board.getBoard()[icol][irow].toString() + BLANK + BAR;
+                }
+            }
+            container += END_LINE;
+            container += HYPHENS_ROW;
+        }
+        System.out.print(container);
     }
 
 }
--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -9,13 +9,7 @@ class Main {
     }
 
     public static void main(final String[] args) {
-        Position pos = new Position(0, 0);
-        King k = new King(Color.BLACK, pos);
-        Queen q = new Queen(Color.WHITE, pos);
-        Pawn p = new Pawn(Color.WHITE, pos);
-        Rook r = new Rook(Color.BLACK, pos);
-        Bishop b = new Bishop(Color.BLACK, pos);
-        Horse h = new Horse(Color.WHITE, pos);
-        ChessBoard board = new ChessBoard();
+        GameConsole console = new GameConsole();
+        console.drawChessBoard();
     }
 }
