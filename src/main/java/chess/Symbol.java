0554b3198c1d3073d686b5b90a53f777ed87ee90
Samuel Loza <starsaminf@gmail.com>
2020-08-22 18:12:19 -0400

    Added black and white figures ascii

--- src/main/java/chess/Symbol.java
+++ src/main/java/chess/Symbol.java
@@ -1,5 +1,32 @@
 package chess;
 
 enum Symbol {
-    p, B, R, H, Q, K, v;
+    P("P", "♙", "♟"),
+    B("B", "♗", "♝"),
+    R("R", "♖", "♜"),
+    H("H", "♘", "♞"),
+    Q("Q", "♕", "♛"),
+    K("K", "♔", "♚");
+
+    private String label;
+    private String white;
+    private String black;
+
+    Symbol(final String label, final String white, final String black) {
+        this.label = label;
+        this.white = white;
+        this.black = black;
+    }
+
+    public String getlabel() {
+        return label;
+    }
+
+    public String getWhite() {
+        return white;
+    }
+
+    public String getBlack() {
+        return black;
+    }
 }
