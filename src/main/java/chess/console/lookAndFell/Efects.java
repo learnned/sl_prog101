8e6e47b76fe48d5cc3cfaf5dabf7f1789b505e3d
Samuel Loza <starsaminf@gmail.com>
2020-08-22 18:10:57 -0400

    Delete unnecessary files

--- src/main/java/chess/console/lookAndFell/Efects.java
+++
@@ -1,9 +0,0 @@
-package chess.console.lookAndFell;
-
-public class Efects {
-
-    public static String yourTurn(String figure) {
-        //return figure;
-        return "\033[5m" + figure + "\033[1m";
-    }
-}
