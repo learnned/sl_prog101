8c36d60177e2446b8482886e7b73cb062d1d27b5
Samuel Loza <starsaminf@gmail.com>
2020-08-22 14:07:38 -0400

    Add Efects for print in console

--- src/main/java/chess/console/lookAndFell/Efects.java
+++ src/main/java/chess/console/lookAndFell/Efects.java
@@ -1,2 +1,9 @@
-package chess.console.lookAndFell;public class Efects {
+package chess.console.lookAndFell;
+
+public class Efects {
+
+    public static String yourTurn(String figure) {
+        //return figure;
+        return "\033[5m" + figure + "\033[1m";
+    }
 }
