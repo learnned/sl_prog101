e171f2d38adbc40d7b952def5fafcb51a2af8f69
Mirko Romay Ramos <mirko.romay@fundacion-jala.org>
2020-08-23 13:00:14 -0400

    Correcting PR comments to KingTest class

--- src/test/java/chess/KingTest.java
+++ src/test/java/chess/KingTest.java
@@ -1,5 +1,4 @@
 package chess;
-import chess.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -34,12 +33,11 @@ public class KingTest {
     }
 
     @Test
-    public void testKingcanMoveToEmpty() {
+    public void testKingCanMoveToEmpty() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.BLACK, new Position(7, 7), chessBoard);
-        chessBoard.addPiece(k);
-        boolean actual = k.move(new Position(6, 7));
+        chessBoard.addPiece(new King(Color.BLACK, new Position(7, 7), chessBoard));
+        boolean actual = chessBoard.getBoard()[7][7].move(new Position(6, 7));
         assertTrue(actual);
     }
 
@@ -47,11 +45,9 @@ public class KingTest {
     public void testKingCannotMovetoAlly() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.BLACK, new Position(6, 5), chessBoard);
-        chessBoard.addPiece(k);
-        Bishop b = new Bishop(Color.BLACK, new Position(7, 6), chessBoard);
-        chessBoard.addPiece(b);
-        boolean actual = k.move(new Position(6, 5));
+        chessBoard.addPiece(new King(Color.BLACK, new Position(6, 5), chessBoard));
+        chessBoard.addPiece(new Bishop(Color.BLACK, new Position(7, 6), chessBoard));
+        boolean actual = chessBoard.getBoard()[6][5].move(new Position(6, 5));
         assertFalse(actual);
     }
 
@@ -59,11 +55,9 @@ public class KingTest {
     public void testKingCannotMovetoAVoidPositionUnderAttack() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.BLACK, new Position(3, 3), chessBoard);
-        chessBoard.addPiece(k);
-        Bishop b = new Bishop(Color.WHITE, new Position(6, 4), chessBoard);
-        chessBoard.addPiece(b);
-        boolean actual = k.move(new Position(4, 2));
+        chessBoard.addPiece(new King(Color.BLACK, new Position(3, 3), chessBoard));
+        chessBoard.addPiece(new Bishop(Color.WHITE, new Position(6, 4), chessBoard));
+        boolean actual = chessBoard.getBoard()[3][3].move(new Position(4, 2));
         assertFalse(actual);
     }
 
@@ -71,13 +65,10 @@ public class KingTest {
     public void testKingCannotGetMovementsUnderAttack() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King bk = new King(Color.BLACK, new Position(3, 3), chessBoard);
-        chessBoard.addPiece(bk);
-        Rook wr = new Rook(Color.WHITE, new Position(6, 4), chessBoard);
-        chessBoard.addPiece(wr);
-        Bishop wb = new Bishop(Color.WHITE, new Position(3, 4), chessBoard);
-        chessBoard.addPiece(wb);
-        int actual = bk.getPossibleMoves().size();
+        chessBoard.addPiece(new King(Color.BLACK, new Position(3, 3), chessBoard)); // adding a Black King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 4), chessBoard)); // adding a White Rook that attacks all row above the king
+        chessBoard.addPiece(new Bishop(Color.WHITE, new Position(3, 4), chessBoard)); // adding a White Bishop, it usually can be killed by king, but now king cannot because Rook could attack king
+        int actual = chessBoard.getBoard()[3][3].getPossibleMoves().size();
         int expected = 5;
         assertEquals(expected, actual);
     }
@@ -86,49 +77,40 @@ public class KingTest {
     public void testKingCanMovetoEnemy() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(k);
-        Bishop b = new Bishop(Color.BLACK, new Position(0, 1), chessBoard);
-        chessBoard.addPiece(b);
-        boolean actual = k.move(new Position(0, 1));
+        chessBoard.addPiece(new King(Color.WHITE, new Position(0, 0), chessBoard));// adding White King
+        chessBoard.addPiece(new Bishop(Color.BLACK, new Position(0, 1), chessBoard));// adding a Black Bishop
+        boolean actual = chessBoard.getBoard()[0][0].move(new Position(0, 1));
         assertTrue(actual);
     }
 
     @Test
-    public void testBlackKingMovesQueenSideCastling() {
+    public void testBlackKingMovesQueensideCastling() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.BLACK, new Position(4, 7), chessBoard);
-        chessBoard.addPiece(k);
-        Rook r = new Rook(Color.BLACK, new Position(0, 7), chessBoard);
-        chessBoard.addPiece(r);
-        boolean actual = k.move(new Position(2, 7));
+        chessBoard.addPiece(new King(Color.BLACK, new Position(4, 7), chessBoard));// adding a Black King
+        chessBoard.addPiece(new Rook(Color.BLACK, new Position(0, 7), chessBoard));// adding an allied Left Rook
+        boolean actual = chessBoard.getBoard()[4][7].move(new Position(2, 7)); // King and Rook can do a Castling
         assertTrue(actual);
     }
 
     @Test
-    public void testBlackKingMovesKingSideCastling() {
+    public void testBlackKingMovesKingsideCastling() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.BLACK, new Position(4, 7), chessBoard);
-        chessBoard.addPiece(k);
-        Rook r = new Rook(Color.BLACK, new Position(7, 7), chessBoard);
-        chessBoard.addPiece(r);
-        boolean actual = k.move(new Position(6, 7));
+        chessBoard.addPiece(new King(Color.BLACK, new Position(4, 7), chessBoard));// adding a Black King
+        chessBoard.addPiece(new Rook(Color.BLACK, new Position(7, 7), chessBoard));// adding an allied Right Rook
+        boolean actual = chessBoard.getBoard()[4][7].move(new Position(6, 7));// King and Rook can do a Castling
         assertTrue(actual);
     }
 
     @Test
-    public void testBlackKingAddsQueenSideAndKingSideCastling() {
+    public void testBlackKingAddsQueensideAndKingsideCastling() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.BLACK, new Position(4, 7), chessBoard);
-        chessBoard.addPiece(k);
-        Rook leftR = new Rook(Color.BLACK, new Position(0, 7), chessBoard);
-        chessBoard.addPiece(leftR);
-        Rook rightR = new Rook(Color.BLACK, new Position(7, 7), chessBoard);
-        chessBoard.addPiece(rightR);
-        int actual = k.getPossibleMoves().size();
+        chessBoard.addPiece(new King(Color.BLACK, new Position(4, 7), chessBoard));// adding a Black King
+        chessBoard.addPiece(new Rook(Color.BLACK, new Position(0, 7), chessBoard));// adding an allied Left Rook
+        chessBoard.addPiece(new Rook(Color.BLACK, new Position(7, 7), chessBoard));// adding an allied Right Rook
+        int actual = chessBoard.getBoard()[4][7].getPossibleMoves().size(); //Possible moves of King are 5 normal and +2 for Castling moves
         int expected = 7;
         assertEquals(expected, actual);
     }
@@ -137,11 +119,9 @@ public class KingTest {
     public void testWhiteKingMovesQueenSideCastling() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(k);
-        Rook r = new Rook(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(r);
-        boolean actual = k.move(new Position(2, 0));
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(0, 0), chessBoard));// adding an allied Left Rook
+        boolean actual = chessBoard.getBoard()[4][0].move(new Position(2, 0));// King can do the Castling
         assertTrue(actual);
     }
 
@@ -149,11 +129,9 @@ public class KingTest {
     public void testWhiteKingMovesKingSideCastling() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King k = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(k);
-        Rook r = new Rook(Color.WHITE, new Position(7, 0), chessBoard);
-        chessBoard.addPiece(r);
-        boolean actual = k.move(new Position(6, 0));
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 0), chessBoard));// adding an allied Right Rook
+        boolean actual = chessBoard.getBoard()[4][0].move(new Position(6, 0));// King can do the Castling
         assertTrue(actual);
     }
 
@@ -162,12 +140,10 @@ public class KingTest {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
         King k = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(k);
-        Rook leftR = new Rook(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(leftR);
-        Rook rightR = new Rook(Color.WHITE, new Position(7, 0), chessBoard);
-        chessBoard.addPiece(rightR);
-        int actual = k.getPossibleMoves().size();
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(0, 0), chessBoard));// adding an allied Left Rook
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 0), chessBoard));// adding an allied Right Rook
+        int actual = chessBoard.getBoard()[4][0].getPossibleMoves().size();//Possible moves of King are 5 normal and +2 for Castling moves
         int expected = 7;
         assertEquals(expected, actual);
     }
@@ -176,26 +152,20 @@ public class KingTest {
     public void testKingIsChecked() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King wk = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(wk);
-        Bishop bb = new Bishop(Color.BLACK, new Position(6, 2), chessBoard);
-        chessBoard.addPiece(bb);
-        assertTrue(wk.isChecked());
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Bishop(Color.BLACK, new Position(6, 2), chessBoard));// adding a Black Bishop attacking the King
+        assertTrue( ((King)chessBoard.getBoard()[4][0]).isChecked());// King should be Checked
     }
 
     @Test
     public void testKingCannotAddCastlingWhenIsChecked() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King wk = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(wk);
-        Rook leftR = new Rook(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(leftR);
-        Rook rightR = new Rook(Color.WHITE, new Position(7, 0), chessBoard);
-        chessBoard.addPiece(rightR);
-        Bishop bb = new Bishop(Color.BLACK, new Position(6, 2), chessBoard);
-        chessBoard.addPiece(bb);
-        boolean actual = wk.move(new Position(6, 0));
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(0, 0), chessBoard));// adding an allied Left Rook
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 0), chessBoard));// adding an allied Right Rook
+        chessBoard.addPiece(new Bishop(Color.BLACK, new Position(6, 2), chessBoard));// adding a Black Bishop attacking the King
+        boolean actual = chessBoard.getBoard()[4][0].move(new Position(6, 0));// King cannot do any Castling
         assertFalse(actual);
     }
 
@@ -203,63 +173,51 @@ public class KingTest {
     public void testKingCannotAddCastlingWhenItsLeftPositionIsUnderAttack() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King wk = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(wk);
-        Rook leftR = new Rook(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(leftR);
-        Rook rightR = new Rook(Color.WHITE, new Position(7, 0), chessBoard);
-        chessBoard.addPiece(rightR);
-        Bishop bb = new Bishop(Color.BLACK, new Position(5, 2), chessBoard);
-        chessBoard.addPiece(bb);
-        boolean actual = wk.move(new Position(2, 0));
-        assertFalse(actual);
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(0, 0), chessBoard));// adding an allied Left Rook
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 0), chessBoard));// adding an allied Right Rook
+        chessBoard.addPiece(new Bishop(Color.BLACK, new Position(1, 2), chessBoard));// adding a Black Bishop attacking position in King's left
+        int actual = chessBoard.getBoard()[4][0].getPossibleMoves().size();// Left Castling should not be added to possible moves
+        int expected = 5;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testKingCannotAddCastlingWhenItsRightPositionIsUnderAttack() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King wk = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(wk);
-        Rook leftR = new Rook(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(leftR);
-        Rook rightR = new Rook(Color.WHITE, new Position(7, 0), chessBoard);
-        chessBoard.addPiece(rightR);
-        Bishop bb = new Bishop(Color.BLACK, new Position(7, 2), chessBoard);
-        chessBoard.addPiece(bb);
-        boolean actual = wk.move(new Position(6, 0));
-        assertFalse(actual);
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(0, 0), chessBoard));// adding an allied Left Rook
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 0), chessBoard));// adding an allied Right Rook
+        chessBoard.addPiece(new Bishop(Color.BLACK, new Position(7, 2), chessBoard));// adding a Black Bishop attacking position in King's right
+        int actual = chessBoard.getBoard()[4][0].getPossibleMoves().size();// Right Castling should not be added to possible moves
+        int expected = 5;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testKingCannotAddCastlingWhenTargetLeftPositionIsUnderAttack() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King wk = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(wk);
-        Rook leftR = new Rook(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(leftR);
-        Rook rightR = new Rook(Color.WHITE, new Position(7, 0), chessBoard);
-        chessBoard.addPiece(rightR);
-        Bishop bb = new Bishop(Color.BLACK, new Position(0, 2), chessBoard);
-        chessBoard.addPiece(bb);
-        boolean actual = wk.move(new Position(2, 0));
-        assertFalse(actual);
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(0, 0), chessBoard));// adding an allied Left Rook
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 0), chessBoard));// adding an allied Right Rook
+        chessBoard.addPiece(new Rook(Color.BLACK, new Position(2, 7), chessBoard));// adding a Black Rook attacking target Left Castling position
+        int actual = chessBoard.getBoard()[4][0].getPossibleMoves().size();// Right Castling should not be added to possible moves
+        int expected = 6;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testKingCannotAddCastlingWhenTargetRightPositionIsUnderAttack() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.crateAnEmptyChessBoard();
-        King wk = new King(Color.WHITE, new Position(4, 0), chessBoard);
-        chessBoard.addPiece(wk);
-        Rook leftR = new Rook(Color.WHITE, new Position(0, 0), chessBoard);
-        chessBoard.addPiece(leftR);
-        Rook rightR = new Rook(Color.WHITE, new Position(7, 0), chessBoard);
-        chessBoard.addPiece(rightR);
-        Bishop bb = new Bishop(Color.BLACK, new Position(4, 2), chessBoard);
-        chessBoard.addPiece(bb);
-        boolean actual = wk.move(new Position(6, 0));
-        assertFalse(actual);
+        chessBoard.addPiece(new King(Color.WHITE, new Position(4, 0), chessBoard));// adding a White King
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(0, 0), chessBoard));// adding an allied Left Rook
+        chessBoard.addPiece(new Rook(Color.WHITE, new Position(7, 0), chessBoard));// adding an allied Right Rook
+        chessBoard.addPiece(new Rook(Color.BLACK, new Position(6, 7), chessBoard));// adding a Black Rook attacking target Right Castling position
+        int actual = chessBoard.getBoard()[4][0].getPossibleMoves().size();// Right Castling should not be added to possible moves
+        int expected = 6;
+        assertEquals(expected, actual);
     }
 }
