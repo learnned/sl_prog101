c2613d07d4a74c31b4fb8867625bc617e8b61161
Samuel Loza <starsaminf@gmail.com>
2020-08-23 22:30:14 -0400

    Adding menu and support blink ANSI

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -1,6 +1,7 @@
 package chess;
 
 import game.GameConsole;
+import java.util.Scanner;
 
 class Main {
 
@@ -11,7 +12,17 @@ class Main {
     }
 
     public static void main(final String[] args) {
-        GameConsole console = new GameConsole();
+        System.out.println("********************************");
+        System.out.println("******Welcome Chess Game********");
+        System.out.println("********************************");
+        System.out.println("Please select a option for display the board");
+        System.out.println("1.- Mode TEXT");
+        System.out.println("2.- Mode ASCII");
+        Scanner sc = new Scanner(System.in);
+        int modeVisualization = sc.nextByte();
+        System.out.println(modeVisualization);
+        GameConsole console = new GameConsole(modeVisualization == 2 );
+        console.init();
         console.drawChessBoard();
     }
 }
