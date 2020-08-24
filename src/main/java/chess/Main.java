0493ee674cbd6c7c806e61eee6722c28083a0760
Samuel Loza <starsaminf@gmail.com>
2020-08-24 12:15:43 -0400

    Adding event blink and corrections of the colors ANSI

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -21,9 +21,8 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int modeVisualization = sc.nextByte();
         System.out.println(modeVisualization);
-        GameConsole console = new GameConsole(modeVisualization == 2 );
+        GameConsole console = new GameConsole( );
         console.init();
-        console.drawChessBoard();
     }
 
 }
